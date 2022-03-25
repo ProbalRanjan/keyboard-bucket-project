0f0a0748a17d1935c3f37e2d3cc4b53499e8d2f7
Probal Ranjan Paul <probal.ranjan1198@gmail.com>
2022-03-26 00:44:49 +0600

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Products from './component/Products/Products';
+import Shop from './component/Shop/Shop';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Shop></Shop>
+      <Products></Products>
     </div>
   );
 }
